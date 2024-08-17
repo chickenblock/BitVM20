20bf4fbdc83eeee60d5d83d96ebebce29887799e
chickenblock <chickenblock24@gmail.com>
2024-08-17 20:08:06 +0530

    started with script 5 and minor refactoring for script 0

--- src/bitvm20/mod.rs
+++ src/bitvm20/mod.rs
@@ -2,4 +2,4 @@ pub mod script0;
 pub mod script1;
 pub mod script2_3;
 pub mod script4;
-//pub mod script5;
+pub mod script5;
--- src/bitvm20/script0.rs
+++ src/bitvm20/script0.rs
@@ -3,12 +3,12 @@ use crate::treepp::{script, Script};
 use crate::pseudo::OP_CHECKSEQUENCEVERIFY;
 
 // inputs must contain sigantures in reverse order of the public_keys
-pub fn construct_script0(blocks_until : i32, public_keys: Vec<String>) -> Script {
+pub fn construct_script0(blocks_until : i32, public_keys: Vec<[u8; 32]>) -> Script {
     script!{
         {blocks_until} OP_CHECKSEQUENCEVERIFY OP_DROP
         OP_0
-        for pk in public_keys {
-            {pk} OP_CHECKSIGADD
+        for pk in &public_keys {
+            {pk.to_vec()} OP_CHECKSIGADD
         }
         {public_keys.len()} OP_EQUAL
     }
