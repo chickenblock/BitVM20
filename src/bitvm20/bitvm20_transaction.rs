8b8660c9864fd73a88a6f6f5d9a18abd2be212b7
chickenblock <chickenblock24@gmail.com>
2024-08-22 23:17:33 +0530

    bitvm20 transactions now uses G1Affine in its representation

--- src/bitvm20/bitvm20_transaction.rs
+++ src/bitvm20/bitvm20_transaction.rs
@@ -1,11 +1,15 @@
 use num_bigint::BigUint;
+use ark_bn254::{G1Affine, G1Projective, Fq, Fr};
+use ark_ff::BigInt;
+use crate::bitvm20::serde_for_coordinate::{serialize_bn254_element,deserialize_bn254_element};
 
 struct bitvm20_transaction {
-    from_public_key: [u8; 64],
-    to_public_key: [u8; 64],
+    from_public_key: G1Affine,
+    to_public_key: G1Affine,
     value: BigUint,
     from_nonce: u64,
 
     // signature attributes
-    r: [u8; 64], // rx and ry, both in little endian form
+    r: G1Affine, // rx and ry, both in little endian form
+    s: Fr,
 }
