3bf8807c7d58daba892a0da02130ccf8297ba3c1
chickenblock <chickenblock24@gmail.com>
2024-08-23 19:41:26 +0530

    implemented constructors for bitvm20 transaction

--- src/bitvm20/bitvm20_transaction.rs
+++ src/bitvm20/bitvm20_transaction.rs
@@ -2,6 +2,7 @@ use num_bigint::BigUint;
 use ark_bn254::{G1Affine, G1Projective, Fq, Fr};
 use ark_ff::BigInt;
 use crate::bitvm20::serde_for_coordinate::{serialize_bn254_element,deserialize_bn254_element};
+use crate::bitvm20::bitvm20_entry::{bitvm20_entry};
 
 struct bitvm20_transaction {
     from_public_key: G1Affine,
@@ -10,6 +11,23 @@ struct bitvm20_transaction {
     from_nonce: u64,
 
     // signature attributes
-    r: G1Affine, // rx and ry, both in little endian form
+    r: G1Affine,
     s: Fr,
+}
+
+impl bitvm20_transaction {
+    pub fn new(from: &bitvm20_entry, to: &bitvm20_entry, value: &BigUint, r: &G1Affine, s: &Fr) -> bitvm20_transaction {
+        return bitvm20_transaction {
+            from_public_key: from.public_key.clone(),
+            to_public_key: to.public_key.clone(),
+            value: value.clone(),
+            from_nonce: from.nonce,
+            r: r.clone(),
+            s: s.clone(),
+        }
+    }
+
+    pub fn new_unsigned(from: &bitvm20_entry, to: &bitvm20_entry, value: &BigUint) -> bitvm20_transaction {
+        return Self::new(from, to, value, &G1Affine::new_unchecked(Fq::new(BigInt::zero()), Fq::new(BigInt::zero())), &Fr::new(BigInt::zero()));
+    }
 }
