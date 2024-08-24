a95c3c47b04a5611fb8b374c88f9c4dd12c036d9
chickenblock <chickenblock24@gmail.com>
2024-08-24 12:43:31 +0530

    generated eP on the stack

--- src/bitvm20/script5.rs
+++ src/bitvm20/script5.rs
@@ -4,6 +4,8 @@ use crate::bitvm20::utils::{verify_input_data,pop_bytes,data_to_signable_balke3_
 
 use crate::signatures::winternitz::PublicKey;
 
+use crate::hash::blake3::blake3_var_length;
+
 use crate::bn254::curves::{G1Affine, G1Projective};
 use crate::bn254::fq::Fq;
 use crate::bn254::fr::Fr;
@@ -30,5 +32,18 @@ pub fn construct_script1(winternitz_public_key: &PublicKey) -> Script {
         { G1Projective::toaltstack() } // push P back to alt stack
         
         // generate e = h(Rx || M)
+        for _ in (0..36) {// copy Rx to the top of ths stack giving ud Rx || tx on the stack
+            {255} OP_ROLL
+        }
+        { blake3_var_length(220) } // hash (Rx || tx-without the singature attributes)
+        { reorder_blake3_output_for_le_bytes() }
+        { Fr::from_hash() }
+
+        // now e is at the top if the stack
+        { G1Projective::toaltstack() } // pop G1Projective P to the top of the stack
+        { Fr::copy(3) } // bring e to the top of the stack, and P right under it
+        { G1Projective::scalar_mul() } // egenerate eP = e * P
+        { G1Projective::toaltstack() } // push eP to alt stack
+
     }
 }
