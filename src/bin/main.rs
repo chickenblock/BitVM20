62049a2a0fba0801edd6a63bac07e177876decfa
Abhijat Sharma <abhijat92@gmail.com>
2024-09-03 11:59:48 +0530

    updated to build binary executable

---
+++ src/bin/main.rs
@@ -0,0 +1,9 @@
+use bitvm::bitvm20::bitvm20_transaction;
+
+fn main() {
+    // Use functionality from your library
+    bitvm20_transaction::new_unsigned();
+    
+    // Your executable's specific logic
+    println!("Hello from the executable!");
+  }
