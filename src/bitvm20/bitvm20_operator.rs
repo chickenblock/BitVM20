c46e46c0b2ab1db6c7fe9ebb9c2ecd6347a4e0ab
chickenblock <chickenblock24@gmail.com>
2024-09-06 14:19:47 +0530

    pseudocode added

--- src/bitvm20/bitvm20_operator.rs
+++ src/bitvm20/bitvm20_operator.rs
@@ -5,6 +5,7 @@ use super::bitvm20_brodacast_packet::bitvm20_broadcast_packet;
 use super::bitvm20_merkel_tree::{bitvm20_merkel_proof, bitvm20_merkel_tree};
 use super::bitvm20_transaction::{bitvm20_transaction};
 use super::bitvm20_entry::{bitvm20_entry};
+use super::bitvm20_user_transaction::bitvm20_user_transaction;
 use num_traits::Zero;
 
 pub struct bitvm20_operator {
@@ -12,6 +13,8 @@ pub struct bitvm20_operator {
 
     pub state_tree : bitvm20_merkel_tree,      // current state of the tree
     pub tx_history : Vec<bitvm20_transaction>, // all transactions applied so far
+
+    pub tx_on_hold : Option<bitvm20_transaction>, // a transaction for which verification signatures have not been received
 }
 
 impl bitvm20_operator {
@@ -31,12 +34,35 @@ impl bitvm20_operator {
     }
 
     // TODO
-    pub fn post_transaction_and_receive_broadcast(tx : &bitvm20_transaction) -> Option<bitvm20_broadcast_packet> {
-        // validate transaction signature
-        // primary validate transaction
-        // generate merkel proofs
+    // called by the user to generate the transaction object and the broadcast
+    pub fn post_transaction_and_receive_broadcast(utx : &bitvm20_user_transaction) -> Option<bitvm20_broadcast_packet> {
+        // fail if there is a transaction already on hold
+        
+        // generate bitvm20_transaction from user_transaction
 
+        // primary validate transaction
+        // generate merkel proofs, merkel proofs must exist, if a bitvm20_transaction can be created from the user_transaction
+        // validate transaction signature
+
+        // if the above validations pass then the transation can be applied to the current state
+
+        // generate random vector strings of winternitz signatures
+        // generate all execution contexts
+
+        // put the transaction on hold
+
+        // generate broadcast_packet out of bitvm20_transaction and the execution contexts and return it
 
         return None;
+    }
+
+    // TODO
+    // called after the operator received all the necessary signatures
+    pub fn receive_verifier_signatures(signatures : Vec<String>) {
+        // note down verifier signatures for the transaction on hold
+
+        // apply the transaction on hold
+
+        // move the transaction on hold to applied transactions
     }
 }
